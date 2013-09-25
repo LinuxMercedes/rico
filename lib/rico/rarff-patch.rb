5379dd1b187e4556107aab7d964eb2e6da20c89d
LinuxMercedes <LinuxMercedes@gmail.com>
2013-09-25 17:48:01 -0500

    Fixing patch to look more like rarff's original idea

--- lib/rico/rarff-patch.rb
+++ lib/rico/rarff-patch.rb
@@ -2,7 +2,11 @@ require 'rarff'
 
 class Rarff::Attribute
 	def to_arff
-		Rarff::ATTRIBUTE_MARKER + " #{@name} #{@type}"
+		if @type_is_nominal && @type.respond_to?("join")
+			Rarff::ATTRIBUTE_MARKER + " #{@name} #{@type.join(',')}"
+		else
+			Rarff::ATTRIBUTE_MARKER + " #{@name} #{@type}"
+		end
 	end
 end
 
