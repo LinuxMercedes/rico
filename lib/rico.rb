aff9326d4d60da04b555ac31eaa95e05a8c770be
LinuxMercedes <LinuxMercedes@gmail.com>
2013-10-10 22:54:40 -0500

    Menu to choose attributes to partition on

--- lib/rico.rb
+++ lib/rico.rb
@@ -1,6 +1,58 @@
 require 'rarff'
 require './rico/rarff-patch.rb'
 
+def select_attributes(rel)
+	attrs = rel.attributes
+	done = false
+	choices = Array.new(attrs.length, false)
+
+	while not done
+		print "Select attributes to partition on:\n\n"
+
+		1.upto(attrs.length).zip(attrs,choices).each { |i, attr, choice|
+			if choice 
+				print "*"
+			end
+
+			puts i.to_s + ") " + attr.name
+		}
+
+		puts"\na) all attributes"
+		puts "c) clear all choices"
+		puts "d) done choosing attributes"
+		print "> "
+
+		input = $stdin.gets
+		input.chomp!
+
+		# TODO: Deuglify
+		case input
+		when 'a'
+			choices = Array.new(attrs.length, true)
+		when 'c'
+			choices = Array.new(attrs.length, false)
+		when 'd'
+			done = true
+		else
+			begin	
+				idx = Integer(input)
+				case idx
+				when 1..attrs.length
+					idx -= 1
+					choices[idx] = !choices[idx]
+				else
+					puts "Not an attribute!"
+				end
+			rescue ArgumentError
+				puts "Not an attribute!"
+			end
+		end
+		puts 
+	end
+
+	return choices
+end
+
 if $0 == __FILE__ then
 	if ARGV[0]
 		arff_file = ARGV[0]
@@ -13,7 +65,18 @@ if $0 == __FILE__ then
 		puts "Please specify a filename"
 		exit
 	end
+	
+	select_attributes(rel)
 
-	puts rel
+	# fucking magic
+	attr_partitions = 1.upto(3).flat_map{ |n| rel.attributes.combination(n).to_a }
+
+	attr_partitions.each { |attrs|
+		attrs.each { |attr|
+			print attr, ", "
+		}
+		puts 
+	}
 
 end
+
