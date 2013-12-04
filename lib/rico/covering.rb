a9877eea9a2e47415a0e599d7360495ebd4a8848
LinuxMercedes <LinuxMercedes@gmail.com>
2013-12-04 17:34:24 -0600

    Find all minimal coverings

--- lib/rico/covering.rb
+++ lib/rico/covering.rb
@@ -9,6 +9,12 @@ def is_less_than(c1, c2)
 	return c1 == c2
 end
 
+def is_in(part, coverings)
+	return coverings.any? { |covering| 
+		covering.length <= part.length and part[0,covering.length] == covering
+	}
+end
+
 def find_covering(rel, decision, indexes, max_attrs=0)
 	# Get partition for decision attribute
 	dec_partition = get_partitions(rel, decision)
@@ -21,13 +27,12 @@ def find_covering(rel, decision, indexes, max_attrs=0)
 	# starting with combos of size 1
 	attr_combos = (1..max_attrs).map { |n| indexes.combination(n).to_a }.flatten(1)
 
-	# Determine if the chosen partition is a valid covering
-	# Due to the ordering of combinations, the first combination
-	# we find is guaranteed minimal.
+	# Generate a list of valid partitions
+	coverings = Array.new
 	attr_combos.each { |idxs|
 		partition = get_partitions(rel, idxs)
-		return idxs if is_less_than(partition, dec_partition)
+		coverings << idxs if is_less_than(partition, dec_partition) and not is_in(idxs, coverings)
 	}
 
-	return []
+	return coverings
 end
