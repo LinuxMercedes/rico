af6a15b11436ea67a90c310b996766961877568a
mhs294 <mhs294@mst.edu>
2013-12-04 17:26:31 -0600

    implemented is_less_than function

--- lib/rico/covering.rb
+++ lib/rico/covering.rb
@@ -2,12 +2,29 @@ require_relative 'partition.rb'
 
 # TODO: Maybe implement?
 def minimal()
-
+    
 end
 
 # TODO: Actually implement
-def is_less_than(c1, c2)
-	
+def is_less_than(p1, p2)
+    # If partitions are equivalent, return true
+	return true if p1 == p2
+    
+    # Check each element of p1 to see if it is less than p2
+    p1.each { |s1|
+        p2.each { |s2|
+            # continue if s2 contains no attributes from s1,
+            # or if s2 is equivalent to s1
+            next if s2 & s1 == [] || s2 == s1
+            
+            # if s2 does not contain all attributes from s1,
+            # then p1 is not less than p2
+            return false if s1 - s2 != []
+        }
+    }
+    
+    # p1 is less than p2
+    return true
 end
 
 def find_covering(rel, decision, indexes, max_attrs=0)
@@ -24,7 +41,7 @@ def find_covering(rel, decision, indexes, max_attrs=0)
 
 	# Determine if the chosen partition is a valid covering
 	# Due to the ordering of combinations, the first combination
-	# we find is guaranteed minimal.
+	# we find is guaranteed minimal
 	attr_combos.each { |idxs|
 		partition = get_partitions(rel, idxs)
 		return idxs if is_less_than(partition, dec_partition)
